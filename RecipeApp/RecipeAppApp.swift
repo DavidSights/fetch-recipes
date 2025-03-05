44a30a0d15bbe6b1282332cdaf1a8bb3b29600a2
David <davidsights@me.com>
2025-03-05 14:11:04 -0600

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by David Seitz Jr on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
